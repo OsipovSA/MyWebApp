3b1cb194600c8a2cb1a347f15a777d64b27a061e
nwpepper <sergeyosipov-86@yandex.ru>
2019-07-18 21:26:49 +0700

    Добавил класс сервлета, запустил jetty-сервер. Получил свой ответ

---
+++ src/main/java/Frontend.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Sergey on 18.07.2019.
+ */
+public class Frontend extends HttpServlet {
+    private String login = "";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        out.print("servlet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletContext;
+
 /**
  * Created by Sergey on 18.07.2019.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/test");
+        server.start();
+        server.join();
     }
 }
